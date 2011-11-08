3f6038ad20c5e7d8648d4785dcdb8b077a794613
hatone <b1006006@gmail.com>
2011-11-08 16:01:49 +0900

    test php-websocket

--- server/server.php
+++ server/server.php
@@ -7,7 +7,7 @@ require(__DIR__ . '/lib/SplClassLoader.php');
 $classLoader = new SplClassLoader('WebSocket', __DIR__ . '/lib');
 $classLoader->register();
 
-$server = new \WebSocket\Server('localhost', 8000);
+$server = new \WebSocket\Server('http://hatone.fluxflex.com', 8000);
 $server->registerApplication('echo', \WebSocket\Application\EchoApplication::getInstance());
 $server->registerApplication('time', \WebSocket\Application\TimeApplication::getInstance());
 $server->run();
